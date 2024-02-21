c39c22137e9dcccd740bf7a9b92b0d53653454bc
Janderson <52183534+janserson@users.noreply.github.com>
2024-02-21 14:23:40 +0000

    EndPoint criado

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Curso backend de node com express')
 })
 
 app.get('/oi', function (req, res){
-    res.send("Olá mundo!!");
+    res.send("Olá mundo!!")
+})
+
+//lista de personagens
+const lista = ["Rick Sanchez", "Morty Smith", "Summer Smith"]
+
+app.get('/item', function (req, res){
+    res.send(lista)
 })
 
 app.listen(3000)
