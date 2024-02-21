72302ef5bba9814f906e353dfb00264eda95ce39
Janderson <52183534+janserson@users.noreply.github.com>
2024-02-21 14:38:53 +0000

    EndPoint Read By Id and Create

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
 app.get('/', function (req, res) {
   res.send('Curso backend de node com express')
 })
@@ -12,8 +15,22 @@ app.get('/oi', function (req, res){
 //lista de personagens
 const lista = ["Rick Sanchez", "Morty Smith", "Summer Smith"]
 
+//Read All -> [GET] /item
 app.get('/item', function (req, res){
     res.send(lista)
+})
+
+//Read by ID -> [GET] /item/:id
+app.get('/item/:id', function (req, res){
+  const id = req.params.id
+  res.send(lista[id])
+})
+
+//post
+app.post('/create', function(req, res){
+  const nome = req.body.nome
+  lista.push(nome)
+  res.send(lista)
 })
 
 app.listen(3000)
